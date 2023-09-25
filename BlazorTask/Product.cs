957240f7ab84e3ade16b80f7ebf55fd13c9e8df9
Dobri <dobri_cs@abv.bg>
2023-09-25 09:46:12 +0300

    Initial comit

---
+++ BlazorTask/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorTask
+{
+    public class Product
+    {
+        public int? ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+        public DateTime DeliveryOn { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
